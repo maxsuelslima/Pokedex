1c78d89bbe7bddf8b84121d199c29766955a936d
Maxsuel Lima <maxsuelslima@gmail.com>
2022-05-26 10:37:49 -0300

    Initial Commit

---
+++ Shared/pokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  pokedexApp.swift
+//  Shared
+//
+//  Created by maxsuel on 26/05/22.
+//
+
+import SwiftUI
+
+@main
+struct pokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
